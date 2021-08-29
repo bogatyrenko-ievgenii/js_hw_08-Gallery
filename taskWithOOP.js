6c8ef01eb18d4b11fd10b3f12c7f5a84789d30ef
Jevgenij <jevgenij.bogatyrenko@gmail.com>
2021-08-29 19:06:36 +0300

    improve gallery

--- taskWithOOP.js
+++ taskWithOOP.js
@@ -60,6 +60,7 @@ const openModal = (event) => {
 
     newGallery.modal.classList.add('is-open');
     newGallery.imageInModal.style.width = '300px';
+    newGallery.imageInModal.style.height = 'auto';
     newGallery.imageInModal.setAttribute('src', event.target.dataset.source);
     newGallery.modal.addEventListener('click', clickBtn);
     newGallery.modal.addEventListener('click', clickBackDrop);
